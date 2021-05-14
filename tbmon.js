88f622d2ac8aceb939e5a38508d47c95adfb7680
unknown <you@example.com>
2021-05-14 20:41:31 +0700

    tbmon.js

---
+++ tbmon.js
@@ -0,0 +1,5 @@
+let toán = +prompt('nhập giá trị toán: ');
+let văn = +prompt('nhập giá trị văn: ');
+let anh = +prompt('nhập giá trị anh: ');
+average = ('toán+văn+anh') /3;
+alert("average là: +average");
